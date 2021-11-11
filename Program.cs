c0767d56c30c2c4e6a561333ce617c408fa9baf9
Don Schenck <don.schenck@gmail.com>
2021-11-11 10:22:09 -0500

    Loops back and summarizes the previous 30 days

--- Program.cs
+++ Program.cs
@@ -17,48 +17,51 @@ namespace vac_seen_rollup
             Rollup();
         }
         static void Rollup() {
-
             string yyyymmdd = DateTime.Now.AddDays(-5).ToString("yyyyMMdd");
             Console.WriteLine("Today is {0}", yyyymmdd);
 
             // Get a count of vaccinations for the location ("us").
-            string countryCode = "us";
+            string countryCode = "US";
 
             // Get count by reading Marten event database
             // Open a session for querying, loading, and
             // updating documents
             try {
-                int countForYesterday = 0;
-                //string sampleMartenConnectionString = "host=postgresql;username=postgres;password=7f986df431344327b52471df0142e520;";
-                string cs = Environment.GetEnvironmentVariable("MARTEN_CONNECTION_STRING");
-                Console.WriteLine("Connecting using this string: {0}",cs);
-                DocumentStore docstore = DocumentStore.For(cs);
-
-                // Open a session for querying
-                using (IDocumentSession session = docstore.OpenSession())
+                // Update for the past 30 days
+                for (int i = 30; i >= 0; i--)
                 {
-                    DateTime dt = DateTime.Now.AddDays(-5);
+                    int countForDate = 0;
+                    //string sampleMartenConnectionString = "host=postgresql;username=postgres;password=7f986df431344327b52471df0142e520;";
+                    string cs = Environment.GetEnvironmentVariable("MARTEN_CONNECTION_STRING");
+                    Console.WriteLine("Connecting to Marten event database using this string: {0}", cs);
+                    DocumentStore docstore = DocumentStore.For(cs);
 
-                    Console.WriteLine("About to query...");
-                    //Task<IReadOnlyList<VaccinationEvent>> events = session.Query<VaccinationEvent>().Where(x => x.EventTimestamp.Year == dt.Year && x.EventTimestamp.Month == dt.Month && x.EventTimestamp.Day == dt.Day).ToListAsync();
-                    var events = session.Query<VaccinationEvent>().Where(x => x.CountryCode == "US");
+                    // Open a session for querying
+                    using (IDocumentSession session = docstore.OpenSession())
+                    {
+                        DateTime dt = DateTime.Now.AddDays(i);
 
-                    countForYesterday = events.Count();
-                    Console.WriteLine("Query done, returning {0} objects.", countForYesterday);
+                        Console.WriteLine("About to query...");
+                        var events = session.Query<VaccinationEvent>().Where(x => x.CountryCode == countryCode && x.EventTimestamp.Year == dt.Year && x.EventTimestamp.Month == dt.Month && x.EventTimestamp.Day == dt.Day);
+                        //var events = session.Query<VaccinationEvent>().Where(x => x.CountryCode == "US");
+
+                        countForDate = events.Count();
+                        Console.WriteLine("Query done, returning {0} objects for {1}.", countForDate, yyyymmdd);
+                    }
+
+                    // UPSERT MariaDB database
+                    string upsertCmd = string.Format("INSERT INTO vaccination_summaries (location_code,reporting_date,vaccination_count) VALUES('{0}','{1}',{2}) ON DUPLICATE KEY UPDATE vaccination_count = {3}", countryCode, yyyymmdd, countForDate, countForDate);
+                    Console.WriteLine("Updating table vaccination_summaries with this statement: {0}", upsertCmd);
+                    //string sampleMySqlConnectionString = "Server=mysql;User ID=root;Password=admin;Database=vaxdb";
+                    using (var connection = new MySqlConnection(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING")))
+                    {
+                        connection.Open();
+                        using (var command = new MySqlCommand(upsertCmd, connection))
+                            command.ExecuteNonQuery();
+                    }
                 }
-
-                // UPSERT MariaDB database
-                string upsertCmd = string.Format("INSERT INTO vaccination_summaries (location_code,reporting_date,vaccination_count) VALUES('{0}','{1}',{2}) ON DUPLICATE KEY UPDATE vaccination_count = {3}", countryCode, yyyymmdd, countForYesterday, countForYesterday);
-                Console.WriteLine("Updating table vaccination_summaries with this statement: {0}", upsertCmd);
-                //string sampleMySqlConnectionString = "Server=mysql;User ID=root;Password=admin;Database=vaxdb";
-                using (var connection = new MySqlConnection(Environment.GetEnvironmentVariable("MYSQL_CONNECTION_STRING")))
-                {
-                    connection.Open();
-                    using (var command = new MySqlCommand(upsertCmd, connection))
-                        command.ExecuteNonQuery();
-                }
-            } catch (Exception e) {
-                throw e;
+            } catch (Exception) {
+                throw;
             }
         }
     }
